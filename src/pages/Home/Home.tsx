701821af8f645d31835e235fd07e4ab4686fd34d
Andrzej Gebus <andrzejgebuss@gmail.com>
2021-08-06 11:12:57 +0200

    Styled component section test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import { Section } from "./components/HomeSection.styled"
 
 function Home(): JSX.Element {
     return (
-        <div>
-
-        </div>
+        <Section></Section>
     )
 }
 
---
+++ src/pages/Home/components/HomeSection.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    width:100vw;
+    min-height:200px;
+    padding:1.5rem;
+`;
