fa28e55f10da1e7483f898340cea0485f3a2ad4e
Andrzej Gebus <andrzejgebuss@gmail.com>
2021-08-12 13:07:24 +0200

    Created private route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 // import { Counter } from './features/counter/Counter';
 import Home from './pages/public/Home/Home';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 
 
 function App(): JSX.Element {
   return (
     <Router>
       <Route path="/" exact component={Home} />
+      <PrivateRoute path="/2" exact component={Home} isAuthenticated={false} />
     </Router>
   );
 }
