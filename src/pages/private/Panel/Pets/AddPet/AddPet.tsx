785d0fc64f3cb1cf8e23385fe435eba3051a079b
Patryk Maj <maj.patryk.gr@gmail.com>
2021-08-23 11:26:27 +0200

    add pet validation

--- src/pages/private/Panel/Pets/AddPet/AddPet.tsx
+++ src/pages/private/Panel/Pets/AddPet/AddPet.tsx
@@ -14,94 +14,91 @@ import { ReactComponent as SVG_Weight } from '../../../../../assets/svg/weight.s
 
 function AddPet(): JSX.Element {
     return (
-        <>
-            <Formik
-                initialValues={{
-                    Name: '',
-                    Sex: '',
-                    Race: '',
-                    Species: '',
-                    File: "",
-                    BirthDay: null,
-                    Color: "",
-                    Weight: "",
-                    Sterilization: "",
-                    Description: ""
-                }}
-                validationSchema={Yup.object({
-                    Name: Yup.string().required('wymagane'),
-                    Sex: Yup.string().required('wymagane'),
-                    Race: Yup.string().required('wymagane'),
-                    Species: Yup.string().required('wymagane'),
-                    File: Yup.object().required('wymagane'),
-                    BirthDay: Yup.date().required('wymagane'),
-                    Color: Yup.string().required('wymagane'),
-                    Weight: Yup.string().required('wymagane'),
-                    Sterilization: Yup.string().required('wymagane'),
-                    Description: Yup.string().required('wymagane')
-                })}
-                onSubmit={values => {
-                    alert(JSON.stringify(values, null, 2));
-                    console.log(values)
-                }}
-            >
-                <Form>
-                    <Title>Ważne informacje</Title>
+        <Formik
+            initialValues={{
+                Name: '',
+                Sex: '',
+                Race: '',
+                Species: '',
+                File: '',
+                BirthDay: null,
+                Color: '',
+                Weight: '',
+                Sterilization: '',
+                Description: ''
+            }}
+            validationSchema={Yup.object({
+                Name: Yup.string().required('wymagane'),
+                Sex: Yup.string().required('wymagane'),
+                Race: Yup.string().required('wymagane'),
+                Species: Yup.string().required('wymagane'),
+                File: Yup.object(),
+                BirthDay: Yup.date().required('wymagane').typeError('Proszę wprowadzić datę(DD-MM-RRRR)'),
+                Color: Yup.string().required('wymagane'),
+                Weight: Yup.number().required('wymagane').typeError('Niepoprawny typ danych, proszę wprowadzić liczbę'),
+                Sterilization: Yup.string().required('wymagane'),
+                Description: Yup.string().min(50, 'Minimum 50 znaków.').max(500, 'Maksymalnie 500 znaków.')
+            })}
+            onSubmit={values => {
+                alert(JSON.stringify(values, null, 2));
+                console.log(values)
+            }}
+        >
+            <Form>
+                <Title>Ważne informacje</Title>
 
-                    <GridContainer>
+                <GridContainer>
 
-                        <GridItem>
-                            <SelectInput name="Species" label="Wybierz gatunek zwierzaka" map={petSpecies}></SelectInput>
-                        </GridItem>
+                    <GridItem>
+                        <SelectInput name="Species" label="Wybierz gatunek zwierzaka" map={petSpecies}></SelectInput>
+                    </GridItem>
 
-                        <GridItem>
-                            <TextInput name="Race" label="Rasa"></TextInput>
-                        </GridItem>
+                    <GridItem>
+                        <TextInput name="Race" label="Rasa"></TextInput>
+                    </GridItem>
 
-                        <GridItem>
-                            <TextInput name="Name" type="text" label="Imię zwierzaka" />
-                        </GridItem>
+                    <GridItem>
+                        <TextInput name="Name" type="text" label="Imię zwierzaka" />
+                    </GridItem>
 
-                        <GridItem>
-                            <SelectInput name="Sex" label="Wybierz płeć" map={petSexes}></SelectInput>
-                        </GridItem>
+                    <GridItem>
+                        <SelectInput name="Sex" label="Wybierz płeć" map={petSexes}></SelectInput>
+                    </GridItem>
 
-                        <GridItem>
-                            <FileInput name="File" label="Dodaj zdjęcie / galerię" type="file" multiple accept="image/*" />
-                        </GridItem>
+                    <GridItem>
+                        <FileInput name="File" label="Dodaj zdjęcie / galerię" type="file" multiple accept="image/*" />
+                    </GridItem>
 
-                    </GridContainer>
+                </GridContainer>
 
-                    <Title>Podstawowe informacje</Title>
+                <Title>Podstawowe informacje</Title>
 
-                    <GridContainer>
+                <GridContainer>
 
-                        <GridItem>
-                            <DateInput name="BirthDay" label="Data Urodzenia"></DateInput>
-                            {/* <TextInput name="BirthDay" label="Data"></TextInput> */}
-                        </GridItem>
+                    <GridItem>
+                        <DateInput name="BirthDay" label="Data Urodzenia"></DateInput>
+                    </GridItem>
 
-                        <GridItem>
-                            <TextInput name="Color" label="Umaszczenie"></TextInput>
-                        </GridItem>
+                    <GridItem>
+                        <TextInput name="Color" label="Umaszczenie"></TextInput>
+                    </GridItem>
 
-                        <GridItem>
-                            <TextInput name="Weight" type="text" label="Waga (w kg)" svg={SVG_Weight}/>
-                        </GridItem>
+                    <GridItem>
+                        <TextInput name="Weight" type="text" label="Waga (w kg)" svg={SVG_Weight} />
+                    </GridItem>
 
-                        <GridItem>
-                            <SelectInput name="Sterilization" label="Czy zwierzak jest wysterylizowany?" map={petSterilization}></SelectInput>
-                        </GridItem>
+                    <GridItem>
+                        <SelectInput name="Sterilization" label="Czy zwierzak jest wysterylizowany?" map={petSterilization}></SelectInput>
+                    </GridItem>
 
-                        <GridItem colspan="2">
-                            <TextArea name="Description" label="Opis"></TextArea>
-                        </GridItem>
+                    <GridItem colspan="2">
+                        <TextArea name="Description" label="Opis"></TextArea>
+                    </GridItem>
 
-                    </GridContainer>
-                    <Button type="submit">Dodaj Zwierzaka</Button>
-                </Form>
-            </Formik>
-        </>
+                </GridContainer>
+                <Button type="submit">Dodaj Zwierzaka</Button>
+            </Form>
+        </Formik>
     )
 }
 
